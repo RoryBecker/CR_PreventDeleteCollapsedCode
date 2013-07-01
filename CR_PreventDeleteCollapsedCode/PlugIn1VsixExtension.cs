90895afd56d3567a9d897b3f7f23d13477083f2f
Rory Becker <RoryBecker@gmail.com>
2013-07-01 20:17:56 +0100

    Initial Load.

---
+++ CR_PreventDeleteCollapsedCode/PlugIn1VsixExtension.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.Composition;
+using DevExpress.CodeRush.Common;
+
+namespace CR_PreventDeleteCollapsedCode
+{
+    [Export(typeof(IVsixPluginExtension))]
+    public class CR_PreventDeleteCollapsedCodeExtension : IVsixPluginExtension { }
+}
